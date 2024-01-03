dae84fe8209dc7a831f3f6412807a7d115c8cfbd
Jon Gordon <jon@jongor.software>
2024-01-03 20:17:20 +0000

    Problem 876 - Middle of the Linked List

---
+++ 876.middle-of-the-linked-list.c
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=876 lang=c
+ *
+ * [876] Middle of the Linked List
+ *
+ * https://leetcode.com/problems/middle-of-the-linked-list/description/
+ *
+ * algorithms
+ * Easy (76.87%)
+ * Likes:    10868
+ * Dislikes: 330
+ * Total Accepted:    1.6M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, return the middle node of the linked
+ * list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we
+ * return the second one.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the list is in the range [1, 100].
+ * 1 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) {
+    /* Use two pointers, one twice as fast as normal stepping. When "fast" drops off the list, "slow" will be in the middle */
+    struct ListNode *slow = head, *fast = head;
+    while (fast && fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+// @lc code=end
+
